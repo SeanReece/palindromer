169735fff5332fe15274532b185c2b7808cce0cb
Sean Reece <reece.sean@gmail.com>
2015-08-01 00:02:26 -0400

    Add support for multi line palindromes. Ignore case
    Remove spaces from a message to check for palindromeness. Make text
    uppercase so that case is not used in comparison.

--- util/palindrome.js
+++ util/palindrome.js
@@ -1,6 +1,7 @@
 //Checks whether the string is a palindrome. 
 //Returns: true if palindrome, false otherwise
 module.exports.isPalindrome = function(text){
+	text = text.replace(/\s+/g, '').toLowerCase();	//Remove spaces to support long, multi-word palindromes. Ignore case
 	var reverse = text.split("").reverse().join("");
 	return text === reverse;
 }
