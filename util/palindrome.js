ff606fb026cb5ccb2b62cab8d2ec1d5b8a44addc
Sean Reece <reece.sean@gmail.com>
2015-08-02 10:56:07 -0400

    Remove special characters from palindrome check

--- util/palindrome.js
+++ util/palindrome.js
@@ -1,7 +1,11 @@
 //Checks whether the string is a palindrome. 
 //Returns: true if palindrome, false otherwise
 module.exports.isPalindrome = function(text){
-	text = text.replace(/\s+/g, '').toLowerCase();	//Remove spaces to support long, multi-word palindromes. Ignore case
+	if(!text)
+		return false;
+	//Remove spaces to support long, multi-word palindromes. Ignore case. Remove special characters
+	text = text.replace(/\s+/g, '').toLowerCase().replace(/[^a-z]/g, '');
+	console.log(text);
 	var reverse = text.split("").reverse().join("");
 	return text === reverse;
 }
