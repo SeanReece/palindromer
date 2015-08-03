39e89376d8dbaa760b119b70f8d5b20985f19018
Sean Reece <reece.sean@gmail.com>
2015-08-02 20:41:00 -0400

    Default mongo to use localhost for travis tests

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
 	port: 8080,
 	debug: true,
 	db: {
-		server: 'palindromer-mongo'		//If mongo is running in a docker container on the same host as application
-		//server: 'localhost'           //If mongo is running natively on the same host as the application
+		//server: 'palindromer-mongo'		//If mongo is running in a docker container on the same host as application
+		server: 'localhost'           //If mongo is running natively on the same host as the application
 		//server: '<MONGO_IP_ADDRESS>'  //If mongo is running on a different server
 	}
 }
