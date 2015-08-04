e5ff3b3ff2ae0432be7fefcdf61a937a39c9835f
Sean Reece <reece.sean@gmail.com>
2015-08-04 09:09:35 -0400

    Add Support for Docker Autobuild
    Change the Docker container build script to use the Docker Hub autobuild image.

--- docker_node/create_node_container.sh
+++ docker_node/create_node_container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -p 80:8080 -e NODE_ENV=production --link palindromer-mongo --restart=always seanreece/palindromer-node-cent
+docker run -d -p 80:8080 -e NODE_ENV=production --link palindromer-mongo:palindromer-mongo --restart=always seanreece/palindromer
